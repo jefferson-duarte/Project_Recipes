64376343537b8dae01c50a5aa692a752210eddbf
Jefferson Duarte <jeffersonsduarte@live.com>
2024-04-29 10:51:15 +0100

    Moved all test to folder tests

--- recipes/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-
-class RecipeURLSsTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
---
+++ recipes/tests/test_recipe_views.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from recipes import views
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
