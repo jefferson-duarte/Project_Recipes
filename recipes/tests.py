8f8959a9793636f1558f7ca65eaab3825e7fc4e8
Jefferson Duarte <jeffersonsduarte@live.com>
2024-04-29 09:16:48 +0100

    Configuring unit tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RecipeURLSsTest(TestCase):
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
