a5e4d7a87df2d381b2a391efa19414dfaa2be938
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-01 10:24:44 +0100

    Created test search by title

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Recipe
 from django.http import Http404
+from django.db.models import Q
 
 
 def home(request):
@@ -57,10 +58,19 @@ def search(request):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True,
+    ).order_by('-id')
+
     return render(
         request,
         'recipes/pages/search.html', {
             'page_title': f'Search for "{search_term}" |',
             'search_term': search_term,
+            'recipes': recipes,
         }
     )
