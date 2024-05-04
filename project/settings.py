5be054938c5af40a726818f44268027517edefe8
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-04 09:57:46 +0100

    Created app authors

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # myapps
-    'recipes'
+    'recipes',
+    'authors',
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls'))
+    path('', include('recipes.urls')),
+    path('authors/', include('authors.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
