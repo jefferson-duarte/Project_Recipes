6c75d3fd692a314015a2ab3ba5e1a4dfddee31d5
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-07 22:53:25 +0100

    Created login authors

--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.urls import reverse
 
@@ -11,7 +11,7 @@ def register_view(request):
 
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
-        'form_action': reverse('authors:create')
+        'form_action': reverse('authors:register_create')
     })
 
 
@@ -33,3 +33,15 @@ def register_create(request):
         del (request.session['register_form_data'])
 
     return redirect('authors:register')
+
+
+def login_view(request):
+    form = LoginForm()
+    return render(request, 'authors/pages/login.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
+
+
+def login_create(request):
+    return render(request, 'authors/pages/login.html')
