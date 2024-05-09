e94e2c32b387ae4d23bf97b016caf00229e7f014
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-09 10:25:09 +0100

    Added functional test for recipe search input

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -59,6 +59,19 @@ class RecipeMixin:
             is_published=is_published,
         )
 
+    def make_recipe_in_batch(self, qtd=10):
+        recipes = []
+        for i in range(qtd):
+            kwargs = {
+                'title': f'Recipe Title {i}',
+                'slug': f'r{i}',
+                'author_data': {'username': f'u{i}'}
+            }
+            recipe = self.make_recipe(**kwargs)
+            recipes.append(recipe)
+
+        return recipes
+
 
 class RecipeTestBase(TestCase, RecipeMixin):
     def setUp(self) -> None:
