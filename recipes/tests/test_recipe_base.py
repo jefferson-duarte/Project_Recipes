41616b8e4d6d0318f941b2952f96a473caffb062
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-09 09:16:12 +0100

    Reorganized the functional test files

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from recipes.models import Category, Recipe, User
 
 
-class RecipeTestBase(TestCase):
+class RecipeMixin:
     def make_category(self, name='Category'):
         return Category.objects.create(name=name)
 
@@ -58,3 +58,8 @@ class RecipeTestBase(TestCase):
             preparation_steps_is_html=preparation_steps_is_html,
             is_published=is_published,
         )
+
+
+class RecipeTestBase(TestCase, RecipeMixin):
+    def setUp(self) -> None:
+        return super().setUp()
--- utils/browser.py
+++ utils/browser.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from time import sleep
+import os
 
 ROOT_DIR = Path(__file__).parent.parent
 CHROMEDRIVER_NAME = 'chromedriver.exe'
@@ -14,6 +15,9 @@ def make_chrome_browser(*options):
     if options is not None:
         for option in options:
             chrome_options.add_argument(option)
+
+    if os.environ.get('SELENIUM_HEADLESS') == '1':
+        chrome_options.add_argument('--headless')
 
     chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
     browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
