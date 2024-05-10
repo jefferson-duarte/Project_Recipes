9ee6fce29c5c0e5be0c1db3eb5dae1a33786a738
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-10 10:53:34 +0100

    Added home pagination functional test

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -40,3 +40,23 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
             title_needed,
             self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
         )
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        self.make_recipe_in_batch()
+
+        # User open the page
+        self.browser.get(self.live_server_url)
+
+        # See that there is a pagination and click in the page 2
+        page2 = self.browser.find_element(
+            By.XPATH,
+            '//a[@aria-label="Go to page 2"]'
+        )
+        page2.click()
+
+        # See that there are 2 recipes in the page 2
+        self.assertEqual(
+            len(self.browser.find_elements(By.CLASS_NAME, 'recipe')),
+            2
+        )
