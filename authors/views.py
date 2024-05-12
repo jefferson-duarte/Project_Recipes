3f6d4a44c0a47dd4f079b6729d7f5688ece08e0c
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-12 10:21:26 +0100

    Created dashboard.html for authors

--- authors/views.py
+++ authors/views.py
@@ -51,7 +51,6 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -67,7 +66,7 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password')
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
@@ -83,3 +82,8 @@ def logout_view(request):
     messages.success(request, 'Logged out successfully.')
     logout(request)
     return redirect(reverse('authors:login'))
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    return render(request, 'authors/pages/dashboard.html')
