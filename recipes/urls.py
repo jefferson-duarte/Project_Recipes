fb47a081511eb3d8fd934180a64aabc00257b176
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-14 19:26:04 +0100

    Updated the Home, Category and Search to use CBV ListView

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,8 +4,17 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.RecipeListViewHome.as_view(), name='home'),
+    path(
+        'recipes/search/',
+        views.RecipeListViewSearch.as_view(),
+        name='search'
+    ),
+    path(
+        'recipes/category/<int:category_id>/',
+        views.RecipeListViewCategory.as_view(),
+        name='category'
+    ),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
-    path('recipes/category/<int:category_id>/', views.category, name='category'),  # noqa
-    path('recipes/search/', views.search, name='search')
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,25 +4,87 @@ from django.http import Http404
 from django.db.models import Q
 from utils.pagination import make_pagination
 import os
+from django.views.generic import ListView
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
-def home(request):
-    recipes = Recipe.objects.filter(
-        is_published=True
-    ).order_by("-id")
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    ordering = ['-id']
+    template_name = 'recipes/pages/home.html'
 
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            is_published=True
+        )
 
-    return render(
-        request,
-        'recipes/pages/home.html',
-        context={
-            'recipes': page_obj,
-            'pagination_range': pagination_range
-        },
-    )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request,
+            ctx.get('recipes'),
+            PER_PAGE
+        )
+
+        ctx.update(
+            {
+                'recipes': page_obj,
+                'pagination_range': pagination_range
+            }
+        )
+
+        return ctx
+
+
+class RecipeListViewHome(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+
+class RecipeListViewCategory(RecipeListViewBase):
+    template_name = 'recipes/pages/category.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            category__id=self.kwargs.get('category_id'),
+        )
+
+        return qs
+
+
+class RecipeListViewSearch(RecipeListViewBase):
+    template_name = 'recipes/pages/search.html'
+
+    def get_queryset(self, *args, **kwargs):
+        search_term = self.request.GET.get('q', '')
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            Q(
+                Q(title__icontains=search_term) |
+                Q(description__icontains=search_term),
+            )
+        )
+
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        search_term = self.request.GET.get('q', '')
+
+        ctx.update(
+            {
+                'page_title': f'Search for "{search_term}" |',
+                'search_term': search_term,
+                'additional_url_query': f'&q={search_term}'
+            }
+        )
+
+        return ctx
 
 
 def category(request, category_id):
