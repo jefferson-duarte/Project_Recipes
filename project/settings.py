8eaa39c160234b7840a544ef8217fd665d6dedbd
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-15 10:15:34 +0100

    Added Django Debug Toolbar

--- project/settings.py
+++ project/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     # myapps
     'recipes',
     'authors',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,3 +145,7 @@ MESSAGE_TAGS = {
     constants.SUCCESS: 'message-success',
     constants.WARNING: 'message-warning',
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
