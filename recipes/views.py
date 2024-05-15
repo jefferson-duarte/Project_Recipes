f4dcf861db2463b01f08d19924ba5fcdb2f8301c
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-15 09:40:45 +0100

    Added JsonResponse in the Home and Detils

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,5 @@
+from django.forms.models import model_to_dict
+from django.http import JsonResponse
 from .models import Recipe
 from django.http import Http404
 from django.db.models import Q
@@ -42,6 +44,19 @@ class RecipeListViewBase(ListView):
 
 class RecipeListViewHome(RecipeListViewBase):
     template_name = 'recipes/pages/home.html'
+
+
+class RecipeListViewHomeApi(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        recipes = self.get_context_data()['recipes']
+        recipes_list = recipes.object_list.values()
+
+        return JsonResponse(
+            list(recipes_list),
+            safe=False,
+        )
 
 
 class RecipeListViewCategory(RecipeListViewBase):
@@ -124,3 +139,25 @@ class RecipeDetails(DetailView):
         })
 
         return ctx
+
+
+class RecipeDetailsApi(RecipeDetails):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
+
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.updated_at)
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + \
+                recipe_dict['cover'].url[1:]
+        else:
+            recipe_dict['cover'] = ''
+
+        del recipe_dict['is_published']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False,
+        )
