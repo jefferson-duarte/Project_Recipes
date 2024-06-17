51842426c02cccea6927bebbf696691ae423fb4f
Jefferson Duarte <jeffersonsduarte@live.com>
2024-06-17 13:59:21 +0100

    Added side menu

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -1,17 +1,44 @@
-function my_scorpe() {
-    const forms = document.querySelectorAll('.form-delete');
+(() => {
+  const forms = document.querySelectorAll('.form-delete');
 
-    for (const form of forms) {
-        form.addEventListener('submit', function (e) {
-            e.preventDefault();
+  for (const form of forms) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
 
-            const confirmed = confirm('Are you sire?');
+      const confirmed = confirm('Are you sire?');
 
-            if (confirmed) {
-                form.submit();
-            }
-        });
-    }
-}
+      if (confirmed) {
+        form.submit();
+      }
+    });
+  }
+})();
 
-my_scorpe();
+(() => {
+  const buttonCloseMenu = document.querySelector('.button-close-menu');
+  const buttonShowMenu = document.querySelector('.button-show-menu');
+  const menuContainer = document.querySelector('.menu-container');
+
+  const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+  const menuHiddenClass = 'menu-hidden';
+
+  const closeMenu = () => {
+    buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+    menuContainer.classList.add(menuHiddenClass);
+  };
+
+  const showMenu = () => {
+    buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+    menuContainer.classList.remove(menuHiddenClass);
+  };
+
+  if (buttonCloseMenu) {
+    buttonCloseMenu.removeEventListener('click', closeMenu);
+    buttonCloseMenu.addEventListener('click', closeMenu);
+  }
+
+  if (buttonShowMenu) {
+    buttonCloseMenu.removeEventListener('click', showMenu);
+    buttonShowMenu.addEventListener('click', showMenu);
+  }
+})();
