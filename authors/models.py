71db4fda16b5e314bc7f37e897e21c8124f6d88b
Jefferson Duarte <jeffersonsduarte@live.com>
2024-06-18 10:35:46 +0100

    Created ProfileView function for the authors

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default='', blank=True)
+
+    def __str__(self):
+
+        return f'{self.author.first_name} {self.author.last_name}'
--- authors/urls.py
+++ authors/urls.py
@@ -25,4 +25,9 @@ urlpatterns = [
         views.DashboardRecipe.as_view(),
         name='dashboard_recipe_edit'
     ),
+    path(
+        'profile/<int:id>/',
+        views.ProfileView.as_view(),
+        name='profile'
+    ),
 ]
